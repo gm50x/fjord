965c30d6516a4c95ef96a26fb95c7cb6c878f5a9
Getúlio Magela Silva <getuliomagela@outlook.com>
2020-05-24 14:26:00 -0300

    simple error handling

--- index.js
+++ index.js
@@ -17,15 +17,8 @@ const { GetAllSamples, GetSampleById } = new Samples()
 app.get('/samples', GetAllSamples.activate)
 app.get('/samples/:id', GetSampleById.activate)
 
+app.use((err, req, res, next) => {
+    res.status(500).json({ error: err.message })
+})
+
 app.listen(8000, () => console.log('Server listening on port 8000'))
-// const Samples = require('./src/Samples')
-
-// const samples = new Samples()
-
-// const result = {
-//     all: samples.GetAllSamples.activate(),
-//     one: samples.GetSampleById.activate(1)
-// }
-
-// console.log(result.all)
-// console.log(result.one)
