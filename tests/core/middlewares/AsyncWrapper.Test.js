b99d75198d240bbe70e776c6506ee4faa47dc85c
Getúlio Magela Silva <getuliomagela@outlook.com>
2020-05-25 17:47:51 -0300

    added AsyncWrapper tests

--- tests/core/middlewares/AsyncWrapper.Test.js
+++ tests/core/middlewares/AsyncWrapper.Test.js
@@ -1,13 +1,15 @@
-const { strictEqual } = require('assert')
+const { strictEqual, doesNotThrow } = require('assert')
 const { AsyncWrapper } = require('../../../src/core/middlewares')
 
 const className = 'AsyncWrapper'
 
-describe(`[incomplete] ${className}`, () => {
-    let instance
+describe.only(`${className}`, () => {
+    let instance, err
     beforeEach(() => {
         instance = new AsyncWrapper()
+        err = undefined
     })
+
     it(`Should instantiate an ${className} object`, () => {
         const actual = instance.constructor.name
         strictEqual(actual, className, `Object is not an instance of ${className}`)
@@ -18,7 +20,15 @@ describe(`[incomplete] ${className}`, () => {
         strictEqual(actual, true, 'Object does not include the wrap method')
     })
 
-    /**
-     * TODO: Implement more tests in this class
-     */
+    it('Should wrap a function', async () => {
+        const actual = (await instance.wrap(() => Promise.resolve('Test'))()) === 'Test'
+        strictEqual(actual, true)
+    })
+
+    it('Should wrap a rejection error', async () => {
+        await (instance.wrap(() => Promise.reject(new Error('Test Error')))())
+            .catch(error => err = error)
+        const actual = err !== undefined
+        strictEqual(actual, true, 'Object does not wrap a promise rejection')
+    })
 })
